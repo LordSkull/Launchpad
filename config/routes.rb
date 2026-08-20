7018dfc1f826d85a4fdb69c8e7c7199e87dbe65b
LordSkull <brunoformisano0@gmail.com>
2026-08-20 14:24:42 +0200

    Remove unused legacy song creation endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   get "/logout" => "application#logout"
   
   #song
-  post "/create_song" => "song#create"
   post "/view_all_songs" => "song#view_all"
   
   get "/google13ecc4458e525973" => "application#google13ecc4458e525973"
